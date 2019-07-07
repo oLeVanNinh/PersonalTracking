5ab73c5a6dc56ebee068c0a1a6d73b45a24a3ce0
ninh <ninhle.9984@gmail.com>
2019-07-07 10:06:44 +0700

    Create task from server

--- server/app/controllers/task_controller.rb
+++ server/app/controllers/task_controller.rb
@@ -1,5 +1,17 @@
 class TaskController < ApplicationController
   def create
-    render json: {hello: "clloud"}
+    task = Task.new(task_params)
+
+    if task.save
+      render json: {status: "success", taks: task}
+    else
+      render json: {status: "false"}
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :total_time, :start_date, :end_date)
   end
 end
