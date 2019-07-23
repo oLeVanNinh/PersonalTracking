44aca3a2b4b12185e5dc098c031db15d612da8b3
ninh <ninhle.9984@gmail.com>
2019-07-23 18:13:03 +0700

    Change deploy branch to master

--- server/config/deploy.rb
+++ server/config/deploy.rb
@@ -26,7 +26,6 @@ set :puma_init_active_record,   true
 set :assets_roles, []
 
 set :scm,                       :git
-set :branch,                    :deploy
 set :branch,                    :master
 set :format,                    :pretty
 set :log_level,                 :debug
@@ -52,8 +51,8 @@ namespace :deploy do
   desc "Make sure local git is sync with remote"
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/deploy`
-        puts "WARNING: HEAD is not the same as origin deploy"
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin master"
         puts "Run git push to sync changes"
         exit
       end
