83916377cc203b25af5502a1fc58bdb80e10dc3e
ninh <ninhle.9984@gmail.com>
2019-07-23 19:10:21 +0700

    Change cors origin to specific domain

--- server/config/application.rb
+++ server/config/application.rb
@@ -33,7 +33,7 @@ module Server
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins ENV["origin_allower"]
         resource '*', header: :any, methods: [:get, :post, :options]
       end
     end
